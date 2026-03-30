8aa838bb123abdec78a53234aedc71c35a22230c
khanttun <khanttun10@gmail.com>
2026-03-30 21:10:40 +0700

    update

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 export const dynamic = "force-dynamic";
 import React from "react";
-import EventForm from "@/components/admin/EventForm";
 
 export default function DashboardEventsPage() {
+  const EventForm = require("@/components/admin/EventForm").default;
   return (
     <main className="p-8 max-w-2xl mx-auto">
       <h1 className="text-2xl font-bold mb-6">Manage Events</h1>
